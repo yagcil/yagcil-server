24515863ae8b76347846161329926d96201b9b0c
Michal Proszek <michal.proszek@gmail.com>
2015-08-03 14:58:53 +0200

    models: Update Task model

--- yagcil/models.py
+++ yagcil/models.py
@@ -5,5 +5,14 @@
 import mongoengine as me
 
 
-class TestModel(me.Document):
-    rotfl = me.StringField(required=True)
+class Task(me.Document):
+    """Task model
+
+    :var key An unique task's id (in Melange database)
+    :var student Name of the student who has finished the task
+    :var categories A list of categories which the task is related to
+    """
+    key = me.IntField(required=True)
+    student = me.StringField(required=True)
+    categories = me.ListField(me.StringField())
+    title = me.StringField(required=True)

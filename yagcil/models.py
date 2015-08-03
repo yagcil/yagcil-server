37066d017bbc2da99d605ab99a7899f1800de9a9
Michal Proszek <michal.proszek@gmail.com>
2015-08-03 15:22:15 +0200

    models: Add year field to Task model

--- yagcil/models.py
+++ yagcil/models.py
@@ -9,10 +9,13 @@ class Task(me.Document):
     """Task model
 
     :var key An unique task's id (in Melange database)
+    :var year The year which the task belongs to
     :var student Name of the student who has finished the task
     :var categories A list of categories which the task is related to
+    :var title Task's title
     """
     key = me.IntField(required=True)
+    year = me.IntField(required=True)
     student = me.StringField(required=True)
     categories = me.ListField(me.StringField())
     title = me.StringField(required=True)

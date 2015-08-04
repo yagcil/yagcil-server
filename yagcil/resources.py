aa128df0482bc5bfc113e007ebbdbcb083abaf00
Michal Proszek <michal.proszek@gmail.com>
2015-08-04 21:18:42 +0200

    resources: Add /task resource #3

--- yagcil/resources.py
+++ yagcil/resources.py
@@ -1,6 +1,8 @@
 """
     API Server resources
 """
+import mongoengine as me
+
 from flask.ext import restful
 from flask.ext.restful import reqparse
 
@@ -46,6 +48,61 @@ class OrganizationResource(restful.Resource):
         ).first().to_dict()
 
 
+class TaskListResource(restful.Resource):
+    """Get a list of all organizations"""
+
+    def __init__(self):
+        self.arg_parser = reqparse.RequestParser()
+        self.arg_parser.add_argument(
+            'org',
+            help="Organization name (default: all tasks)"
+        )
+        self.arg_parser.add_argument(
+            'year',
+            type=int,
+            default=max(app.config['YEARS']),
+            help="GCI year"
+        )
+        self.arg_parser.add_argument(
+            'limit',
+            type=int,
+            help="Results length limit"
+        )
+        self.arg_parser.add_argument(
+            'offset',
+            type=int,
+            default=0,
+            help="Results offset"
+        )
+
+    def get(self):
+        """Get a list of all tasks
+
+        :return list A list of all tasks
+        """
+        args = self.arg_parser.parse_args()
+        org_name = args.get('org')
+        year = args.get('year')
+        limit = args.get('limit')
+        offset = args.get('offset')
+
+        query = Task.objects(year=year)
+        if org_name:
+            try:
+                org = Organization.objects.get(name=org_name, year=year)
+            except me.DoesNotExist:
+                return []
+
+            query = query(org=org)
+
+        if limit is not None:
+            query = query.limit(limit)
+
+        query = query.skip(offset)
+
+        return queryset_to_dict(query)
+
+
 class TaskResource(restful.Resource):
     """Get task's data"""
 
@@ -61,4 +118,5 @@ class TaskResource(restful.Resource):
 
 api.add_resource(OrganizationListResource, '/organization')
 api.add_resource(OrganizationResource, '/organization/<name>/<int:year>')
+api.add_resource(TaskListResource, '/task')
 api.add_resource(TaskResource, '/task/<int:task_id>')

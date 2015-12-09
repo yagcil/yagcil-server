39f5a64ca0373f8375637ec2c21f67183b7e628c
Michal Proszek <michal.proszek@gmail.com>
2015-12-09 19:16:11 +0100

    Update production.py

--- yagcil/config/production.py
+++ yagcil/config/production.py
@@ -5,12 +5,12 @@
 """
 import os
 
-SECRET_KEY = os.environ.get('OPENSHIFT_SECRET_TOKEN')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-MONGODB_DB_NAME = os.environ.get('OPENSHIFT_APP_NAME')  # Database's name is the same as app's
-MONGODB_DB_HOST = os.environ.get('OPENSHIFT_MONGODB_DB_HOST')
-MONGODB_DB_PORT = os.environ.get('OPENSHIFT_MONGODB_DB_PORT')
-MONGODB_DB_USERNAME = os.environ.get('OPENSHIFT_MONGODB_DB_USERNAME')
-MONGODB_DB_PASSWORD = os.environ.get('OPENSHIFT_MONGODB_DB_PASSWORD')
+MONGODB_DB_NAME = os.environ.get('MONGODB_NAME')  # Database's name is the same as app's
+MONGODB_DB_HOST = os.environ.get('MONGODB_HOST')
+MONGODB_DB_PORT = os.environ.get('MONGODB_PORT')
+MONGODB_DB_USERNAME = os.environ.get('MONGODB_USERNAME')
+MONGODB_DB_PASSWORD = os.environ.get('MONGODB_PASSWORD')
 
 YEARS = [2014, 2013, 2012]

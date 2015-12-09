71424d00233659dbd5de40b9cb42779dec79287b
Michal Proszek <michal.proszek@gmail.com>
2015-12-09 19:53:58 +0100

    Update __init__.py

--- yagcil/__init__.py
+++ yagcil/__init__.py
@@ -13,7 +13,7 @@ api = restful.Api(app)
 if os.environ.get('DEBUG', 'true').lower() == 'false':
     app.config.from_object('yagcil.config.production')
     # Set up the database
-    me.connect(host=app.config['MONGODB_URI']))
+    me.connect(host=app.config['MONGODB_URI'])
 else:
     app.config.from_object('yagcil.config.debug')
     # If testing is on, let connect to the database later
